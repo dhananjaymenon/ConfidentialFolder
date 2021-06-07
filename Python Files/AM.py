7b47c7785daebd7492590db880e7dabe8539dd55
dhananjaymenon <72432304+dhananjaymenon@users.noreply.github.com>
2021-06-07 17:41:04 +0400

    Create AM.py

---
+++ Python Files/AM.py
@@ -0,0 +1,50 @@
+#AM stands for Alert Message
+
+import cv2
+import random
+import os
+import smtplib
+from email.message import EmailMessage
+import imghdr
+
+
+cam = cv2.VideoCapture(0)
+n = random.randint(1000, 9999)
+ret, img = cam.read()
+cv2.imshow("Test",img)
+
+k=cv2.waitKey(1000)
+
+file='C://dsg//img//'+str(n)+'.jpg'
+cv2.imwrite(file,img)
+cam.release()
+cv2.destroyAllWindows()
+
+
+#SEND EMAIL
+#INSERT YOUR DETAILS HERE
+email = ""
+password = ""
+enduser = ""
+###################
+
+
+msg = EmailMessage()
+msg['Subject'] = 'SECURITY ALERT'
+msg['From'] = email
+msg['To'] = enduser
+msg.set_content('There was an attempt to log in to your system. This was reported due to an incorrect password entered.' \
+           'If this was you, Ignore the message')
+
+with open('C://dsg//img//'+str(n)+'.jpg','rb') as f:
+    file_data = f.read()
+    file_type = imghdr.what(f.name)
+    file_name = f.name
+
+msg.add_attachment(file_data,maintype='image',subtype=file_type,filename=file_name)
+
+with smtplib.SMTP_SSL('smtp.gmail.com',465) as smtp:
+
+    smtp.login(email,password)
+    smtp.send_message(msg)
+
